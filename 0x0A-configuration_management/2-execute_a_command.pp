4f4d4829cef73a707ecccee3e343a77c1a589715
fourbet <ofourbet@yahoo.fr>
2021-04-02 16:50:15 +0000

    fix(): undo changes

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
-# Create a manifest that kills a process named killmenow
-exec { 'kill':
+# Manifest that kills a process named killmenow
+exec { 'killmenow':
   command => 'pkill killmenow',
-  path => '/usr/bin/',
+  provider => 'shell',
 }
