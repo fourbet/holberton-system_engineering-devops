6cf0fe5281f7c4a44116751c9cd5c71fa7600a95
fourbet <ofourbet@yahoo.fr>
2021-05-03 14:43:36 +0000

    feat(): Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" API project """
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+
+    for id in range(1, 10):
+        user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                            .format(id))
+        employee = user.json()
+        username = employee.get('username')
+
+        todo = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
+                            .format(id))
+        todos = todo.json()
+
+        filename = 'todo_all_employees' + '.json'
+        with open(filename, mode='a') as f:
+            data = {}
+            data[id] = []
+            for task in todos:
+                data[id].append({
+                    'task': task.get('title'),
+                    'completed': task.get('completed'),
+                    'username': username
+                })
+            json.dump(data, f)
