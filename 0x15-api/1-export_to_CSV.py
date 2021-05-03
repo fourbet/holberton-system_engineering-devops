d7ebebfeba8624a5f940408b8c93aeef6fa3a52e
fourbet <ofourbet@yahoo.fr>
2021-05-03 12:53:33 +0000

    feat(): export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" API project """
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(id))
+    employee = user.json()
+    username = employee.get('username')
+
+    todo = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
+                        .format(id))
+    todos = todo.json()
+
+    filename = id + '.csv'
+    with open(filename, mode='w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([id, username, task.get('completed'),
+                             task.get('title')])
