83c7f21b17a9e527f42884156dbb0dc956efccb6
fourbet <ofourbet@yahoo.fr>
2021-05-03 12:32:52 +0000

    feat(): Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" API project """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(id))
+    employee = user.json()
+    name = employee.get('name')
+
+    todo = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
+                        .format(id))
+    todos = todo.json()
+    taskDone = 0
+    taskTotal = 0
+    for task in todos:
+        taskTotal += 1
+        if task.get('completed') == True:
+            taskDone += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, taskDone, taskTotal))
+
+    for task in todos:
+        if task.get('completed') == True:
+            print("\t " + task.get('title'))
+
