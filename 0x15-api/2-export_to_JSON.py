c3492e95a4b6bd82a6e506891ef08b67cfb01364
fourbet <ofourbet@yahoo.fr>
2021-05-03 13:28:21 +0000

    feat(): export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" API project """
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(id))
+    employee = user.json()
+    username = employee.get('username')
+
+    todo = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
+                        .format(id))
+    todos = todo.json()
+
+    filename = id + '.json'
+    with open(filename, mode='w') as f:
+        data = {}
+        data[id] = []
+        for task in todos:
+            data[id].append({
+                'task': task.get('title'),
+                'completed': task.get('completed'),
+                'username': username
+            })
+        json.dump(data, f)
