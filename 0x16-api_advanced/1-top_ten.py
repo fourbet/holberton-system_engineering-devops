9659cf39fab44e8131f74284c5b583a0de83e53e
fourbet <ofourbet@yahoo.fr>
2021-05-18 13:29:56 +0000

    fix(): url

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ import requests
 
 def top_ten(subreddit):
     """queries the Reddit API, prints the titles of the first 10 hot posts"""
-    res = requests.get('https://www.reddit.com/r/{}/top.json?limit=10'
+    res = requests.get('https://www.reddit.com/r/{}/hot.json?limit=10'
                        .format(subreddit),
                        headers={"User-Agent": "My-User-Agent"},
                        allow_redirects=False,
