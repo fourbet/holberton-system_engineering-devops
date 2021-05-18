6dab56001ef6d7572af62b9b1acc06c655e56bd5
fourbet <ofourbet@yahoo.fr>
2021-05-18 14:39:25 +0000

    feat(): result sorted alphabeticaly

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,7 +7,7 @@ def count_words(subreddit, word_list, after=None, count=0, sum_word={}):
     """parses the title of all hot articles, and prints a sorted count of given keywords"""
     if sum_word == {}:
         for word in word_list:
-            sum_word[word] = 0
+            sum_word[word.lower()] = 0
     res = requests.get(
         'https://www.reddit.com/r/{}/hot.json?after={}&count={})'
         .format(subreddit, after, count),
@@ -19,13 +19,12 @@ def count_words(subreddit, word_list, after=None, count=0, sum_word={}):
         title = post.get('data').get('title')
         for word in word_list:
             if word in title:
-                sum_word[word] += 1
+                sum_word[word.lower()] += 1
     after = res.json().get('data').get('after')
     count = res.json().get('data').get('count')
     if not after:
-        sortedDict = dict( sorted(sum_word.items(), key=lambda x: x[0].lower()) )
-        for key, value in sortedDict.items():
-            if value != 0:
-                print('{}: {}'.format(key, value))
+        for key in sorted(sum_word.keys()):
+            if sum_word[key] != 0:
+                print('{}: {}'.format(key, sum_word[key]))
         return
     return count_words(subreddit, word_list, after, count, sum_word)
